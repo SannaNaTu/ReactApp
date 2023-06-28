c5736af840181a2d7b2c49cebb022213251fe1e7
SannaNaTu <31957337+SannaNaTu@users.noreply.github.com>
2023-06-28 11:24:38 +0300

    project start

---
+++ app/index.js
@@ -0,0 +1,13 @@
+import { View, Text } from 'react-native';
+
+const Home = () => {
+    return (
+        <View>
+            <Text>
+                Testi
+            </Text>
+        </View>
+    )
+}
+
+export default Home;
